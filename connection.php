c7246fe46797cfb226b24d0bb2bdf6b0c456ecc0
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 15:22:57 -0700

    update Connection

--- connection.php
+++ connection.php
@@ -1,4 +1,15 @@
 <?php
-//$connection = mysql_connect('localhost', 'root', 'Jarvey16018!');
-//$select_db = mysqli_select_db($connection, 'CC');
+$link = mysqli_connect("127.0.0.1", "root", "Jarvey16018!", "CC");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
 ?>
