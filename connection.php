20d4ea77169c245746eac0acbac556e25fd6a460
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 15:08:09 -0700

    update Connection

--- connection.php
+++ connection.php
@@ -1,10 +1,4 @@
 <?php
 $connection = mysql_connect('localhost', 'root' , 'Jarvey16018!');
-if (!$connection){
-	die("Database Connected Failed" . mysqli_error($connection))
-}
 $select_db = mysqli_select_db($connection, 'CC');
-if (!$select_db){
-	die("Database Selection Failed" . mysqli_error($select_db))
-}
 ?>
