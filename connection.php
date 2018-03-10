76f288f208feca0609203062410c1e5bbfcdae59
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 15:15:40 -0700

    update Connection

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$connection = mysql_connect('localhost', 'root', 'Jarvey16018!');
+//$connection = mysql_connect('localhost', 'root', 'Jarvey16018!');
 //$select_db = mysqli_select_db($connection, 'CC');
 ?>
