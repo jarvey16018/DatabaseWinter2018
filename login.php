8325bfacf8758e81a5097c45b26ddb36f96b3d5f
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 20:08:37 -0700

    update Connection

--- login.php
+++ login.php
@@ -1,1 +1,65 @@
-login.php
+
+
+<?php
+session_start();
+require_once('connection.php');
+if(isset($_POST) & !empty($_POST)){
+	
+	//print_r($_POST);
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	//echo $username;
+
+	echo $sql = "INSERT INTO 'login' (username, password) VALUES ('$username', '$password')";
+	$query = "SELECT * FROM 'login' WHERE username = '$username' AND password = '$password'";
+	$result = mysqli_query($link, $query);
+	if($result){
+		echo "Valid";
+	}else{
+		echo "Failed";
+	}
+
+
+
+	//$count = mysqli_num_rows($result);
+
+	//if($count ==1){
+		//$SESSION['username'] =b$username;
+	//}
+	//else
+	//{
+		//$fmsg = "Invalid Login";
+	//}
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	  <div class="container">
+     	 <form class="form-signin" method="POST">
+         <h2 class="form-signin-heading">Please Login</h2>
+         <div class="input-group">
+	         <span class="input-group-addon" id="basic-addon1">@</span>
+	         <input type="text" name="username" class="form-control" placeholder="Username" required>
+	     </div>
+         <label for="inputPassword" class="sr-only">Password</label>
+         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+         <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+         <a class="btn btn-lg btn-primary btn-block" href="register.php">Register</a>
+         </form>
+      <div>
+</body>
+</html>
