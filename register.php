148f7d306f617c0fbd17f7b3d1caa9395ab2abb9
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 18:54:18 -0700

    update Connection

--- register.php
+++ register.php
@@ -2,19 +2,14 @@
 <?php
 require_once('connection.php');
 //if(isset($_POST) & !empty($_Post)){
+	
 	$username = $_Post['username'];
-	$username =  'Test';
 	$password = $_Post['inputPassword'];
-	echo $username;
+	//echo $username;
 
 	echo $sql = "INSERT INTO 'login' (username, password) VALUES ('$username', '$password')";
-
-
 //}
-
-
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
