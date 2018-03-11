acba0d6d840f31b28b4508c74d903e88d3d47a7a
Jason Harvey <jarvey16018@me.cpm>
2018-03-10 20:10:27 -0700

    update Connection

--- login.php
+++ login.php
@@ -10,8 +10,8 @@ if(isset($_POST) & !empty($_POST)){
 	$password = $_POST['password'];
 	//echo $username;
 
-	echo $sql = "INSERT INTO 'login' (username, password) VALUES ('$username', '$password')";
-	$query = "SELECT * FROM 'login' WHERE username = '$username' AND password = '$password'";
+	//echo $sql = "INSERT INTO 'login' (username, password) VALUES ('$username', '$password')";
+	echo $query = "SELECT * FROM 'login' WHERE username = '$username' AND password = '$password'";
 	$result = mysqli_query($link, $query);
 	if($result){
 		echo "Valid";
