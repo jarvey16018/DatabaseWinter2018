a35ecbd09eb4b68ee520ec73f6d05e4c4216341e
Jason Harvey <jarvey16018@me.cpm>
2018-03-11 20:53:36 -0600

    update of Hello

--- Hello.php
+++ Hello.php
@@ -1,6 +1,17 @@
 
 
 
+
 <?php
+
 session_start();
- echo $_SESSION['username']; ?>
+
+if (!isset($_SESSION['username'])) {
+	header('Location: login.php');
+
+ }
+?>
+
+<?php
+echo "HELLO WORLD"
+?>
