bbf8e7276460297d6aaedff0166e8c297e4518d0
Jason Harvey <jarvey16018@me.cpm>
2018-03-11 20:22:43 -0600

    update of Hello

--- Hello.php
+++ Hello.php
@@ -1,3 +1,11 @@
 <?php
+//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("location: login.php");
+}
+?>
+
+<?php
 echo "HELLO WORLD"
 ?>
