87e0678ee6306a266da40b8744cab554ddef14da
Jason Harvey <jarvey16018@me.cpm>
2018-03-12 15:58:35 -0600

    update

--- Hello.php
+++ Hello.php
@@ -9,6 +9,36 @@ if (!isset($_SESSION['username'])) {
  }
 ?>
 
-<?php
-echo "HELLO WORLD"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+	  <div class="container">
+	  	 <?php if(isset($smsg)){?><div class="alert alert-success" role="alert"> <?php echo $smsg; ?> </div> 	
+	  	 <?php } ?>
+	  	  <?php if(isset($fmsg)){?><div class="alert alert-danger" role="alert"> <?php echo $fmsg; ?> </div> 	
+	  	 <?php } ?>
+     	 <form class="form-signin" method="POST">
+         <h2 class="form-signin-heading">Please Login</h2>
+         <div class="input-group">
+	         <span class="input-group-addon" id="basic-addon1">@</span>
+	         <input type="text" name="username" class="form-control" placeholder="Username" required>
+	     </div>
+         <label for="inputPassword" class="sr-only">Password</label>
+         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+         <button class="btn btn-lg btn-primary btn-block" type="submit">submit</button>
+         <a class="btn btn-lg btn-primary btn-block" href="logout.php">Logout</a>
+         </form>
+      <div>
+</body>
+</html>
