d13b3f243580a10da2dd2b63fc10b2f7f55eb952
Jason Harvey <jarvey16018@me.cpm>
2018-03-12 16:44:55 -0600

    update

--- connection2.php
+++ connection2.php
@@ -1,10 +1,10 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'Jarvey16018!');
+$connection2 = mysqli_connect('localhost', 'root', 'Jarvey16018!');
 if (!$connection){
-    die("Database Connection Failed" . mysqli_error($connection));
+    die("Database Connection Failed" . mysqli_error($connection2));
 }
-$select_db = mysqli_select_db($connection, 'Vinyl');
+$select_db = mysqli_select_db($connection2, 'Vinyl');
 if (!$select_db){
-    die("Database Selection Failed" . mysqli_error($connection));
+    die("Database Selection Failed" . mysqli_error($connection2));
 }
 ?>
