617acc4405fcc36bd742326c01e4770830314fe3
Jason Harvey <jarvey16018@me.cpm>
2018-03-12 16:04:54 -0600

    update

--- Hello.php
+++ Hello.php
@@ -32,10 +32,10 @@ if (!isset($_SESSION['username'])) {
          <h2 class="form-signin-heading">Add Inventory</h2>
          <div class="input-group">
 	         <span class="input-group-addon" id="basic-addon1">Product Name</span>
-	         <input type="text" name="username" class="form-control" placeholder="Username" required>
+	         <input type="text" name="productname" class="form-control" placeholder="Product Name" required>
 	     </div>
-         <label for="inputPassword" class="sr-only">Password</label>
-         <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+         <span class="input-group-addon" id="basic-addon1">Color</span>
+	         <input type="text" name="color" class="form-control" placeholder="Color" required>
          <button class="btn btn-lg btn-primary btn-block" type="submit">submit</button>
          <a class="btn btn-lg btn-primary btn-block" href="logout.php">Logout</a>
          </form>
