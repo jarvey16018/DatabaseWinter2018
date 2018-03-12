d3ce0e5016e4da5c488fe7569cfe43a464916ff2
Jason Harvey <jarvey16018@me.cpm>
2018-03-11 20:32:22 -0600

    update of Hello

--- Hello.php
+++ Hello.php
@@ -8,9 +8,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 //PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
 
-if (!isset($_SESSION['username'])) {
+//if (!isset($_SESSION['username'])) {
     header('Location: login.php');
-}
+//}
 ?>
 
 <?php
