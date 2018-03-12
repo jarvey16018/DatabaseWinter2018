a5a7b4fe16e53634f797073acee496e41a621f06
Jason Harvey <jarvey16018@me.cpm>
2018-03-11 20:48:15 -0600

    update of Hello

--- Hello.php
+++ Hello.php
@@ -1,19 +1,4 @@
 
 
 
-
-<?php
-
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
-//PUT THIS HEADER ON TOP OF EACH UNIQUE PAGE
-
-if (!isset($_SESSION['username'])) {
-	header('Location: login.php');
-	exit();
-}
-?>
-
-<?php
-echo "HELLO WORLD"
-?>
+<?php echo $_SESSION['username']; ?>
