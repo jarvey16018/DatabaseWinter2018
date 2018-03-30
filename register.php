906d29b6375921a29bd3fb9f0d79a4da276f9fbf
Jason Harvey <jarvey16018@me.cpm>
2018-03-29 21:40:41 -0600

    update of Hello

--- register.php
+++ register.php
@@ -8,10 +8,10 @@ require_once('Connection4.php');
 if(isset($_POST) & !empty($_POST)){
  	$username = mysqli_real_escape_string($connection, $_POST['username']);
  	$password = md5($_POST['password']);
- 	$firstName = md5($_POST['firstName']);
- 	$lastName = md5($_POST['lastName']);
- 	$email = md5($_POST['email']);
- 	$Address = md5($_POST['Address']);
+ 	$firstName = mysqli_real_escape_string($_POST['firstName']);
+ 	$lastName = mysqli_real_escape_string($_POST['lastName']);
+ 	$email = mysqli_real_escape_string($_POST['email']);
+ 	$Address = mysqli_real_escape_string($_POST['Address']);
 
 
  	$sql = "INSERT INTO `Just` (username, password, firstName, lastName, email, Address) VALUES ('$username', '$password', '$firstName', '$lastName', '$email', '$Address')";
