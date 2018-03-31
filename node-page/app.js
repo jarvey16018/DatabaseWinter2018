a522478deea932c79aa96f8fe8e0857a59c65a8d
Jason Harvey <jarvey16018@me.cpm>
2018-03-30 20:24:17 -0600

    update of Hello

--- node-page/app.js
+++ node-page/app.js
@@ -22,7 +22,7 @@ app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("rating saved to database");
+            res.send("rating submitted");
         })
         .catch(err => {
             res.status(400).send("Unable to save to database");
