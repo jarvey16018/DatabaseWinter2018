42edb8a1476b9681a00c912306bc255f7573cdaf
Jason Harvey <jarvey16018@me.cpm>
2018-04-17 20:58:55 -0600

    update2

--- mongobackup.py
+++ mongobackup.py
@@ -1,3 +1,5 @@
 import shutil
-shutil.copytree('/var/lib/mongodb', '/var/www/DatabaseWinter2018/backups')
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+shutil.copytree('/var/lib/mongodb', '/var/www/DatabaseWinter2018/backups'+timestr)
 
