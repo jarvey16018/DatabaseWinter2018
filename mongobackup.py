e8fa1944d3d769a962b2d70ee74b750f71f51726
Jason Harvey <jarvey16018@me.cpm>
2018-04-17 20:54:55 -0600

    update2

--- mongobackup.py
+++ mongobackup.py
@@ -1,14 +1,3 @@
-import os
-import os.path
 import shutil
-import fnmatch
-list_of_dirs_to_copy = '/var/lib/mongodb' # List of source dirs
-dest_dir = '/var/www/DatabaseWinter2018/backups'     # folder for the destination of the copy
-files_patterns = ['*.txt', '*.doc']
-for root_path in list_of_dirs_to_copy:
-    for root, dirs, files in os.walk(root_path): # recurse walking
-        if not os.path.exists(dest_dir):
-            os.makedirs(dest_dir)  # create the dir if not exists
-        for pattern in files_patterns:
-            for thefile in fnmatch.filter(files, pattern):  # filter the files to copy
-                shutil.copy2(os.path.join(root, thefile), dest_dir) #copy file
+shutil.copytree('/var/lib/mongodb', '/var/www/DatabaseWinter2018/backups')
+
